2d9641d1fa1233e79565828bde7d8d09e0b9ce0b
Russell <russell.zy.lin@gmail.com>
2026-01-23 16:37:53 +0800

    Add LeGoat class
    Rename Duke.java to LeGoat.java.
    Add LeGoat ASCII Art as logo.

---
+++ src/main/java/LeGoat.java
@@ -0,0 +1,14 @@
+public class LeGoat {
+    public static void main(String[] args) {
+        String logo = """
+                       _      ___    ____    _____     ___    _______
+                      | |    |  _|  / ___\\  / / \\ \\   / _ \\  |__   __|
+                      | |    | |_  / / ___  | | | |  / /_\\ \\    | |
+                      | |    |  _| \\ \\|_  \\ | | | | / /   \\ \\   | |
+                      | |___ |_|_   \\ \\_| | | \\ / | | |   | |   | |   _
+                      |____/ |___|   \\___/  \\_____/ |_|   |_|   |_|  |_|
+                      """;
+        System.out.println("Hello from\n" + logo + "\n" + "What can I do for you?");
+        System.out.println("LeGoat logging off!");
+    }
+}
