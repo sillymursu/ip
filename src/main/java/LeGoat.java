d1a9e8585fe0082594a7be7e87e69b73cfbeb009
Russell <russell.zy.lin@gmail.com>
2026-01-25 20:42:38 +0800

    Add Echo in LeGoat.java
    LeGoat now echoes input from Scanner, quits when input is "bye".

--- src/main/java/LeGoat.java
+++ src/main/java/LeGoat.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class LeGoat {
     public static void main(String[] args) {
         String logo = """
@@ -5,10 +7,24 @@ public class LeGoat {
                       | |    |  _|  / ___\\  / / \\ \\   / _ \\  |__   __|
                       | |    | |_  / / ___  | | | |  / /_\\ \\    | |
                       | |    |  _| \\ \\|_  \\ | | | | / /   \\ \\   | |
-                      | |___ |_|_   \\ \\_| | | \\ / | | |   | |   | |   _
+                      | |___ |_|_   \\ \\_| | | \\_/ | | |   | |   | |   _
                       |____/ |___|   \\___/  \\_____/ |_|   |_|   |_|  |_|
                       """;
-        System.out.println("Hello from\n" + logo + "\n" + "What can I do for you?");
-        System.out.println("LeGoat logging off!");
+        String longLine = "\n--------------------------------------------------\n";
+        String byeMsg = "LeGoat logging off!";
+        System.out.println("Hello from\n" + logo + "\n" + "What can I do for you?" + longLine);
+        @SuppressWarnings("ConvertToTryWithResources")
+        Scanner sc = new Scanner(System.in);
+        int byeFlag = 0;
+        while (byeFlag == 0) {
+            String input = sc.nextLine();
+            if (!input.equals("bye")) {
+                System.out.println(longLine + input + longLine);
+            } else {
+                byeFlag = 1;
+            }
+        }
+        System.out.println(longLine + byeMsg + longLine);
+        sc.close();
     }
 }
