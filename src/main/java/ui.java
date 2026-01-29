c8cf4ac52458e509756c60c1eb4c022479dc65c6
Russell <russell.zy.lin@gmail.com>
2026-01-29 23:10:32 +0800

    Refactor with OOP
    - Add taskHandler
    - Add ui
    - Refactor inputHandler
    - Refactor LeGoat

---
+++ src/main/java/ui.java
@@ -0,0 +1,43 @@
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ui {
+    String longLine = "--------------------------------------------------";
+    String LeGoatStr = "LeGoat: ";
+    String deadlineTimeFormatReminder = """
+
+                                PS: If you want "/by" to be formatted:
+                                    yyyy mm dd <24h time>""";
+    String eventTimeFormatReminder = """
+
+                                PS: If you want "/from" or "/to" to be formatted:
+                                    yyyy mm dd <24h time>""";
+
+    public ui() {}
+
+    public String parseDate(String maybeDate) {
+        try {
+            DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("yyyy MM dd HHmm");
+            DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("MMM yyyy hh:mm a");
+            LocalDateTime inputTime = LocalDateTime.parse(maybeDate, inputFormat);
+            String daySuffix = getDaySuffix(inputTime.getDayOfMonth());
+            String outputTime = String.format("%d%s %s", inputTime.getDayOfMonth(), daySuffix, inputTime.format(outputFormat));
+            return outputTime;
+        } catch (DateTimeException e) {
+            return "";
+        }
+    }
+
+    public String getDaySuffix(int day) {
+        if (day >= 11 && day <= 13) {
+            return "th";
+        }
+        return switch (day % 10) {
+            case 1 -> "st";
+            case 2 -> "nd";
+            case 3 -> "rd";
+            default -> "th";
+        };
+    }
+}
